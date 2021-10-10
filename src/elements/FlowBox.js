87e7ffa8436efce27449ddc784f28cd355bf1817
Kim Syrjälä <kim.syrjala@gmail.com>
2021-10-10 22:15:43 +0200

    Fix FlowBox::appendChild

--- src/elements/FlowBox.js
+++ src/elements/FlowBox.js
@@ -5,5 +5,11 @@ export default class FlowBox extends Widget {
 	get type() {
 		return Gtk.FlowBox;
 	}
+
+	appendChild(child) {
+		if (!this.hasChild(child)) {
+			this.instance.insert(child.instance, -1);
+		}
+	}
 }
 
